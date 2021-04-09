2f7b92133a79b386a2a43af3f8f17d6b9e6cdec1
julianoleo <50123832+julianoleo@users.noreply.github.com>
2021-04-09 19:53:32 -0300

    Exercício Thread - 09/04/2021

---
+++ src/Thread_01/Cliente.java
@@ -0,0 +1,61 @@
+package Thread_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Cliente extends Thread{
+	
+	private static boolean done = false;
+	private Socket conexao;
+	
+	public Cliente(Socket s) {
+		conexao = s;
+	}
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket conexao = new Socket("localhost", 2000);
+		PrintStream saida = new PrintStream(conexao.getOutputStream());
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("Entre com o seu nome:");
+		String meuNome = teclado.readLine();
+		saida.println(meuNome);
+		Thread t = new Cliente(conexao);
+		t.start();
+		String linha;
+		
+		while(true) {
+			if(done) {
+				break;
+			}
+			System.out.println("> ");
+			linha = teclado.readLine();
+			saida.println(linha);
+		}
+	}
+	
+	public void run() {
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+			String linha;
+			while(true) {
+				linha = entrada.readLine();
+				if(linha.trim().equals("")) {
+					System.out.println("Conexao encerrada!!!");
+					break;
+				}
+				System.out.println();
+				System.out.println(linha);
+				System.out.println("---> ");
+			}
+			done = true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/Thread_01/Servidor.java
@@ -0,0 +1,76 @@
+package Thread_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Servidor extends Thread{
+	
+	private static Vector clientes;
+	private Socket conexao;
+	private String meuNome;
+	
+	public Servidor(Socket s) {
+		conexao = s;
+	}
+		
+	public static void main(String[] args) throws IOException {
+		clientes = new Vector();
+		
+		ServerSocket s = new ServerSocket(2000);
+		while(true) {
+			System.out.println("Esperando conectar ... ");
+			Socket conexao = s.accept();
+			System.out.println(" Conectou!");
+			Thread t = new Servidor(conexao);
+			t.start();
+		}
+	}
+	
+	public void run() {
+		try {
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+			PrintStream saida = new PrintStream(conexao.getOutputStream());
+			meuNome = entrada.readLine();
+			
+			if(meuNome == null) {
+				return;
+			}
+			
+			clientes.add(saida);
+			String linha = entrada.readLine();
+			
+			while((linha != null) && (!linha.trim().equals(""))) {
+				sendToAll(saida, " disse: ", linha);
+				linha = entrada.readLine();
+			}
+			
+			sendToAll(saida," saiu "," do chat!");
+			clientes.remove(saida);
+			conexao.close();
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void sendToAll(PrintStream saida, String acao, String linha) {
+		Enumeration e = clientes.elements();
+		while(e.hasMoreElements()) {
+			PrintStream chat = (PrintStream) e.nextElement();
+			if(chat != saida) {
+				chat.println(meuNome + acao + linha);
+			}
+			if(acao == " saiu ") {
+				if(chat == saida) {
+					chat.println("");
+				}
+			}
+		}
+	}
+}
